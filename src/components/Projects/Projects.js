b0cc78b9a864ac6dc789ce2c57d3acd947ee9dd0
MW_coder <138982171+theMuhammad@users.noreply.github.com>
2024-08-12 16:34:41 +0500

    edited

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -23,7 +23,6 @@ function Projects() {
               imgPath={ggeek}
               isBlog={false}
               title="Game Geek"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
               ghLink="https://github.com/theMuhammad/Geek-game"
               demoLink="https://geek-game.vercel.app/"
             />
@@ -34,7 +33,6 @@ function Projects() {
               imgPath={country}
               isBlog={false}
               title="Countiries"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
               ghLink="https://github.com/theMuhammad/Country"
               demoLink="https://countries-git-main-muhammads-projects-61b561d4.vercel.app/"
             />
@@ -45,7 +43,6 @@ function Projects() {
               imgPath={billz}
               isBlog={false}
               title="Billz"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
               ghLink=""
               demoLink="https://project-billz.netlify.app/store"              
             />
